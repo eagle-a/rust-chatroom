47b6377acb254719861166ef715288c5f02e62da
eagle-a <2121612901@qq.com>
2023-11-17 16:06:13 +0800

    bat批处理自动推送:周五 2023/11,16:06:13

--- chatroom/src/main.rs
+++ chatroom/src/main.rs
@@ -1,8 +1,9 @@
+
 #![windows_subsystem = "windows"]
 mod uiapp;
 mod data;
 mod send_and_get;
 
 fn main() {
-    uiapp::app_main_ui();
+    uiapp::app_login();
 }
--- chatroom/src/uiapp.rs
+++ chatroom/src/uiapp.rs
@@ -10,10 +10,57 @@ const GRAY: Color = Color::from_hex(0x757575);
 const WIDTH: i32 = 600;
 const HEIGHT: i32 = 400;
 
-pub fn app_sign_in() {
-}
+pub fn app_sign_in() {}
 
 pub fn app_login() {
+    let app = app::App::default();
+    app::background(255, 255, 255);
+    app::set_visible_focus(false);
+    let mut window = window::Window::default()
+        .with_size(400, 300)
+        .with_label("Login and Registration");
+
+    let username_entry =
+        input::IntInput::new(100, 50, 100, 20, "username").set_label_color(Color::Blue);
+    let password_entry =
+        input::IntInput::new(100, 100, 100, 20, "password").set_label_color(Color::Blue);
+    let mut submit_btn =
+        button::Button::new(180, 120, 60, 25, "Submit").with_align(Align::Center | Align::Inside);
+
+    // username_label.set_frame(FrameType::FlatBox);
+    // username_label.set_label_color(Color::White);
+    // username_label.set_color(BLUE);
+    // username_label.draw(|b| {
+    //     draw::set_draw_rgb_color(211, 211, 211);
+    //     draw::draw_rectf(0, b.height(), b.width(), 3);
+    // });
+
+    submit_btn.set_callback(move |submit_btn| {
+    let app_wrong = app::App::default();
+    app::background(255, 255, 255);
+    app::set_visible_focus(false);
+    let mut window_wrong = window::Window::default()
+        .with_size(200, 100)
+        .with_label("Login and Registration");
+    let mut bar =
+    frame::Frame::new(0, 0, WIDTH, 60, " 密码错误").with_align(Align::Left | Align::Inside);
+    window_wrong.end();
+    window_wrong.make_resizable(true);
+    window_wrong.show();
+    app_wrong.run().unwrap();
+    });
+
+    let mut login_group = group::Group::default()
+        .with_size(300, 200)
+        .center_of(&window); // Create a group to align widgets vertically
+
+    login_group.add(&submit_btn);
+    login_group.end(); // End the group and align the widgets vertically
+
+    window.add(&login_group); // Add the group to the window
+    window.show(); // Show the window
+
+    app.run().unwrap(); // Run the application and wait for events to occur
 }
 
 pub fn app_main_ui() {

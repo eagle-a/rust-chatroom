1862e49293ab0e4ab630ab9cc44361da49b306bd
eagle-a <2121612901@qq.com>
2023-12-31 21:28:06 +0800

    bat批处理自动推送:周日 2023/12,21:28:06

--- chatroom/src/main.rs
+++ chatroom/src/main.rs
@@ -4,8 +4,16 @@ mod send_and_get;
 
 mod uiapp;
 mod ui_components;
-fn main() {
-  //  ui_components::login::training();
+use fltk::{prelude::*, *};
+use fltk_theme::{WidgetScheme, SchemeType};
 
-    let a: bool = uiapp::ui_run();
+fn main() {
+    let a = app::App::default();
+    let widget_scheme = WidgetScheme::new(SchemeType::Clean);
+    widget_scheme.apply();
+    let mut win = window::Window::default().with_size(400, 300);
+    let mut btn = button::Button::new(160, 200, 80, 30, "Hello");
+    win.end();
+    win.show();
+    a.run().unwrap();
 }
